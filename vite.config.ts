131d769976496328a0fe8d4d745a9f3b56a84700
Mariusz Najwer <najwer23@gmail.com>
2024-05-25 13:09:50 +0200

    Vitest for JSX

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: "/react-starter/",
 	plugins: [react()],
+	test: {
+		globals: true,
+		environment: "jsdom",
+		setupFiles: ["./src/test/setup.ts"],
+	},
 	build: {
 		outDir: "./build",
 
