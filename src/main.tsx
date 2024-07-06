9b8d00f963c18d5f0c724a83704f22d45af181d9
Mariusz Najwer <najwer23@gmail.com>
2024-07-06 23:32:26 +0200

    Tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
-import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { App } from './App'
 import './main.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(<App />)
